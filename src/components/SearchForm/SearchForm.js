8176f49babfcb98b7351d30c787f26ca6c2b8a72
Евгений Моисеев <eugen.moiseev@gmail.com>
2022-10-13 23:15:24 +0300

    ДП.L2. Исправление ошибок по ревью 2

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ function SearchForm() {
       <form className='search-form__content' name="search">
         <div className="search-form__block">
           <div className="search-form__logo"></div>
-          <input className="search-form__input" value="Фильм" name="search" type="text" required />
+          <input className="search-form__input" placeholder="Фильм" name="search" type="text" required />
           <button className="search-form__button" type="submit"></button>
         </div>
         <div className="search-form__toggle">
